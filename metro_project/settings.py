a1c3b61a6274259c032817236082ecf24928bb18
LucasJavaudin <lucas.javaudin@gmail.com>
2021-04-20 15:31:55 +0200

    use pathlib for BASE_DIR

--- metro_project/settings.py
+++ metro_project/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from pathlib import Path
 import os
 import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
 # Read settings from settings.json file.
 settings_filename = os.path.join(BASE_DIR, 'settings.json')
