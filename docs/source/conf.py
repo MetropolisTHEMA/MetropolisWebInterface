f51466cc8a716d9b461023696355513226e98261
= <=>
2022-06-30 12:29:40 +0200

    fixing bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,22 @@ extensions = ['sphinx.ext.autodoc',
               #'sphinxcontrib.apidoc',
               ]
 
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
 apidoc_module_dir = "/Users/ndiayeabass/Desktop/Metropolis/metroweb/metro/metro_app/"
 apidoc_output_dir = "/Users/ndiayeabass/Desktop/Metropolis/metroweb/metro/docs/source"
 apidoc_excluded_paths = "/Users/ndiayeabass/Desktop/Metropolis/metroweb/metro/metro_app/migrations"
